1311ebb310cb9c2a06d29b2eb7ea01c1a7091090
shinra420070186-crypto <shinra420070186@gmail.com>
2026-03-24 20:28:49 +0530

    Initialize main.jsx with React and GameBoard

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import GameBoard from './GameBoard';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <GameBoard />
+  </React.StrictMode>
+);
